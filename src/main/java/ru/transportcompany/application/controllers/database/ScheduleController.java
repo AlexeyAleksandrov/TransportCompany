aacb2032a9f649ef46ec566689263ab7a5070302
AlexeyAleksandrov <56308724234w@gmail.com>
2023-05-19 00:27:39 +0300

    Начато создание редактирования расписания

--- src/main/java/ru/transportcompany/application/controllers/database/ScheduleController.java
+++ src/main/java/ru/transportcompany/application/controllers/database/ScheduleController.java
@@ -83,22 +83,16 @@ public class ScheduleController
     public String addScheduleItemPage(@RequestParam String date, Model model)
     {
         // маршруты
-        List<Route> routes = new ArrayList<>();
+        List<Route> routes = null;
         try
         {
-            // System.out.println("Дата: " + date);
-
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             Date searchDate = dateFormat.parse(date);
 
             model.addAttribute("date", searchDate);
 
-            // System.out.println("Дата: " + searchDate.toString() + " День: " + searchDate.getDate() + " День недели: " + searchDate.getDay());
-
             // ищем подходящие маршруты на выбранный день
             routes = routesService.getRoutesForDate(searchDate);
-
-            model.addAttribute("routes", routes);
 
             // формируем дату
             SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMMM yyyy");
@@ -106,7 +100,12 @@ public class ScheduleController
         }
         catch (ParseException e)
         {
+            routes = new ArrayList<>();
             model.addAttribute("date_error", "Указана некорректная дата!");
+        }
+        finally
+        {
+            model.addAttribute("routes", routes);
         }
 
         // транспорт
@@ -126,5 +125,68 @@ public class ScheduleController
         scheduleRepository.save(schedule);
 
         return "redirect:/schedule/show";
+    }
+
+    @GetMapping(value = "/edit")
+    public String editSchedulePage(Model model)
+    {
+        model.addAttribute("date", LocalDate.now());
+        return "schedule/edit";
+    }
+
+    @PostMapping(value = "/edit")
+    public String editScheduleChosePage(@RequestParam String date, Model model)
+    {
+        List<Schedule> schedules = null;
+
+        try
+        {
+            // перевод строки в дату
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date searchDate = dateFormat.parse(date);
+            model.addAttribute("date", date);
+
+            // формируем дату
+            SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMMM yyyy");
+            model.addAttribute("search_date", outputDateFormat.format(searchDate));
+
+            // расписание
+            schedules = scheduleRepository.findAllByDate(searchDate);
+
+        }
+        catch (ParseException e)
+        {
+            schedules = new ArrayList<>();
+            model.addAttribute("date_error", "Указана некорректная дата!");
+            e.printStackTrace();
+        }
+        finally
+        {
+            model.addAttribute("schedules", schedules);
+        }
+
+        return "schedule/edit_chose";
+    }
+
+    @PostMapping(value = "/edit/{id}")
+    public String editScheduleItemPage(@PathVariable Long id, @ModelAttribute("date") Date date, Model model)
+    {
+        // формируем дату
+        SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMMM yyyy");
+        model.addAttribute("search_date", outputDateFormat.format(date));
+
+        // расписание
+        Schedule schedule = scheduleRepository.findById(id).orElse(null);
+        model.addAttribute("schedule", schedule);
+
+        // ищем подходящие маршруты на выбранный день
+        List<Route> routes = routesService.getRoutesForDate(date);
+        model.addAttribute("routes", routes);
+
+        // транспорт
+        List<Transport> transports = transportRepository.findAll();
+        model.addAttribute("transports", transports);
+
+        return "schedule/edit_item";
     }
 }
